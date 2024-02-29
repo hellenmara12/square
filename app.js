898a8e13e19e96776f0906f162776440d379d012
Adesiyan Temitope <adesiyantope2014@gmail.com>
2024-02-29 09:02:29 +0100

    first

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const express = require("express");
 require('dotenv').config();
 const axios = require('axios');
+const cors = require('cors');
 const app = express();
+
+app.use(cors());
 
 app.get("/", (req, res) => {
   res.status(200).json({ message: "Express on Vercel"})
